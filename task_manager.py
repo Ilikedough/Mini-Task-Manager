5cbd161d57f855496826e761d1e672533ed9414c
ItsDough <itsdough0610@gmail.com>
2026-07-09 14:55:37 +0100

    Add TaskManager with add, delete, edit functions

--- task_manager.py
+++ task_manager.py
@@ -24,3 +24,35 @@ class Task():
     id: uuid.UUID = field(default_factory=uuid.uuid4)
     
 
+class TaskManager():
+    def __init__(self) -> None:
+        self.tasks = []
+    
+    def add(self,task) -> bool:
+        if isinstance(task,Task):
+            if task in self.tasks:
+                return False
+            self.tasks.append(task)
+            return True
+        return False
+    
+    def delete(self,task) -> bool:
+        if isinstance(task,Task):
+            if task in self.tasks:
+                self.tasks.remove(task)
+                return True
+            return False
+        return False
+
+    def edit(self,task,info) -> bool:
+        if not isinstance(task,Task):
+            return False
+        properties = ("name","description","owner","progress","priority")
+        for prop in properties:  
+            if prop in info:
+                if prop in ("progress","priority"):
+                    if not prop in (Progress, Priority):
+                        return False
+                setattr(task,prop,info[prop])
+        return True
+        
