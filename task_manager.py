39c00f572427979215d9d5db32076b57abb0e605
ItsDough <itsdough0610@gmail.com>
2026-07-09 14:34:41 +0100

    Add Enum for progress and priority

--- task_manager.py
+++ task_manager.py
@@ -1,13 +1,26 @@
 from dataclasses import (dataclass,field)
+from enum import Enum
 import uuid
+
+
+class Progress(Enum):
+    TODO = "TODO"
+    IN_PROGRESS = "IN_PROGRESS"
+    DONE = "DONE"
+
+class Priority(Enum):
+    LOW = "Low"
+    MEDIUM = "Medium"
+    HIGH = "High"
+
 
 @dataclass(kw_only=True)
 class Task():
     name: str
     description: str
     owner: str
-    progress: str = "TODO"
-    priority: str = "Low"
+    progress: Progress = Progress.TODO
+    priority: Priority = Priority.LOW
     id: uuid.UUID = field(default_factory=uuid.uuid4)
     
 
