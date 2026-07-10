a19018f17972d88b8d4a64046ff477ebf901d636
ItsDough <itsdough0610@gmail.com>
2026-07-10 15:06:34 +0100

    refactor: improve logger setup

--- logger.py
+++ logger.py
@@ -1,26 +1,25 @@
 import logging
 
 
-def setup_logger(name, filename = "log.log"):
+def setup_logger(name, filename="log.log"):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
 
+    if logger.handlers:
+        return logger
 
     console_handler = logging.StreamHandler()
-    file_handler = logging.FileHandler(filename)
+    console_handler.setLevel(logging.WARNING)
 
-    formatter = logging.Formatter("%(asctime) - %(filename) - %(funcName) - %(levelname) - %(message)")
+    file_handler = logging.FileHandler(filename, encoding="utf-8")
+    file_handler.setLevel(logging.INFO)
+
+    formatter = logging.Formatter("%(asctime)s - %(filename)s:%(lineno)d - %(funcName)s - %(levelname)s - %(message)s")
 
     console_handler.setFormatter(formatter)
     file_handler.setFormatter(formatter)
 
-    console_handler.setLevel(logging.WARNING)
-    file_handler.setLevel(logging.INFO)
-
-    logger = logging.getLogger(name)
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    if logger.handlers:
-        return logger
-    
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
 
