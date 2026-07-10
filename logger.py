492138b6efe91684d51ca23aa7759e1dec9e81de
ItsDough <itsdough0610@gmail.com>
2026-07-10 14:33:27 +0100

    Add a logger file

---
+++ logger.py
@@ -0,0 +1,27 @@
+import logging
+
+
+def setup_logger(name, filename = "log.log"):
+
+
+    console_handler = logging.StreamHandler()
+    file_handler = logging.FileHandler(filename)
+
+    formatter = logging.Formatter("%(asctime) - %(filename) - %(funcName) - %(levelname) - %(message)")
+
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
+
+    console_handler.setLevel(logging.ERROR)
+    file_handler.setLevel(logging.INFO)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    if logger.handlers:
+        return logger
+    
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
+    return logger
